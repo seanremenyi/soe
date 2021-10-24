8d17f4c0dd043704fc57ce9ee89239b2d60ae3ca
Sean <sean.remenyi@gmail.com>
2021-10-24 22:02:35 +0000

    puch

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,4 @@ sudo yum update -y
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "This is the ultimate test <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▄&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▄    <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▌▒█&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▄▀▒▌   <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▌▒▒█&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▄▀▒▒▒▐   <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▐▄█▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐   <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐ &nbsp&nbsp&nbsp&nbsp&nbsp such content <br />
-&nbsp&nbsp&nbsp▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌   <br />
-&nbsp&nbsp▐▒▒▒▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▌  <br />
-&nbsp&nbsp▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐  <br />
-&nbsp▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌ <br />
-&nbsp▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌ &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp much wow<br />
-▌▒▒▒▄██▄▒▒▒▒▒▒▒▒░░░░░░░░▒▒▒▐ <br />
-▐▒▒▐▄█▄█▌▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▒▌<br />
-▐▒▒▐▀▐▀▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▐ <br />
-&nbsp▌▒▒▀▄▄▄▄▄▄▀▒▒▒▒▒▒▒░▒░▒░▒▒▒▌ <br />
-&nbsp▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▒▄▒▒▐  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp nice website <br /> 
-&nbsp&nbsp▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▄▒▒▒▒▌  <br />
-&nbsp&nbsp&nbsp&nbsp▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀   <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀     <br />
-&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp▀▀▀▀▀▀▀▀▀▀▀▀        " > /var/www/html/index.html
+echo "This is the ultimate test <br /> " > /var/www/html/index.html
